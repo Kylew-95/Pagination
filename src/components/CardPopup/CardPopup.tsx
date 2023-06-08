d98afa6779b7d4e5ea4a40a9900363561e1c24cf
Kyle <kylejordanwilliams@gmail.com>
2023-06-08 11:01:07 +0100

    Successfully completed pagination with card hover effects

--- src/components/CardPopup/CardPopup.tsx
+++ src/components/CardPopup/CardPopup.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { CardPopupProps } from "../TypescriptTypes";
 import { Button } from "@mui/material";
-import ".//Cardpopup.css";
+import "./Cardpopup.css";
 
 function CardPopup({ poksData, onClose }: CardPopupProps) {
   if (!poksData) {
     return null; // Render nothing if poksData is null
+  }
+
+  function readFlavorText() {
+    if (poksData?.flavorText) {
+      const utterance = new SpeechSynthesisUtterance(poksData.flavorText);
+      speechSynthesis.cancel(); // Cancel any previous speech synthesis in progress
+      speechSynthesis.speak(utterance);
+    }
   }
 
   function handlesUrl() {
@@ -17,15 +25,32 @@ function CardPopup({ poksData, onClose }: CardPopupProps) {
       <div className="overlay"></div>
       <h3 className="card-popup-content">CLICK ON THE CARD TO FIND OUT MORE</h3>
       <div className="card-popup">
-        <img
-          id="largeSize"
-          src={poksData.images.large}
-          alt={poksData.name}
-          onClick={handlesUrl}
-        />
-        <Button variant="contained" className="close-button" onClick={onClose}>
-          Close
-        </Button>
+        <>
+          <img
+            id="largeSize"
+            src={poksData.images.large}
+            alt={poksData.name}
+            onClick={handlesUrl}
+          />
+          <h1>{poksData.flavorText}</h1>
+        </>
+        <div className="allbtns">
+          <Button
+            variant="contained"
+            className="close-button"
+            onClick={onClose}
+          >
+            Close
+          </Button>
+
+          <Button
+            variant="contained"
+            className="playbtn"
+            onClick={readFlavorText}
+          >
+            Play me
+          </Button>
+        </div>
       </div>
     </>
   );
