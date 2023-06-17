1efd32f52d74b915a14f1442fdc00ad0f5020cd7
Kylew-95 <127200705+Kylew-95@users.noreply.github.com>
2023-06-17 13:25:01 +0100

    Update SuperbaseClient.jsx

--- src/components/SuperBase/SuperbaseClient.jsx
+++ src/components/SuperBase/SuperbaseClient.jsx
@@ -88,9 +88,9 @@ export default function SuperbaseClient({ profile }) {
     }
   };
 
-  const authChange = supabase.auth.onAuthStateChange((event, session) => {
-  console.log(event, session)
-})
+//   const authChange = supabase.auth.onAuthStateChange((event, session) => {
+//   console.log(event, session)
+// })
 
   return (
     <>
