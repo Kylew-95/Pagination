bf92d0b6d905bf7750a92d7496a7c7301236d13d
Kylew-95 <127200705+Kylew-95@users.noreply.github.com>
2023-06-17 13:14:25 +0100

    Update SuperbaseClient.jsx
    Trying to find the cause of why the log out state doesn't remain

--- src/components/SuperBase/SuperbaseClient.jsx
+++ src/components/SuperBase/SuperbaseClient.jsx
@@ -88,6 +88,10 @@ export default function SuperbaseClient({ profile }) {
     }
   };
 
+  const authChange = supabase.auth.onAuthStateChange((event, session) => {
+  console.log(event, session)
+})
+
   return (
     <>
       {user ? (
